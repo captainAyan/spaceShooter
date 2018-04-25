247d4cb515c0cb36967097f9996bec73de2c53fd
captainAyan <33987712+captainAyan@users.noreply.github.com>
2018-04-25 21:00:13 +0530

    Update object.js

--- src/js/object.js
+++ src/js/object.js
@@ -1,5 +1,6 @@
 /// All Classes
 
+
 /**
  * Ship class
  * 
@@ -8,34 +9,53 @@
  * @prop {float | int} radius ship's radius.
  *
  */
-function Ship(x,y,radius) {
+function Ship(x,y) {
     this.position = {
         x:x,
         y:y
     }
-    this.radius = radius;
+    this.radius = 7;
     this.color = "#d5d5d5";
-    
+    this.shipImage = document.getElementById('ship');
+
+    // ship's flame
+    this.flame = {
+        // the image
+        sprite : document.getElementById('flame'),
+        // where on the sprite you want to draw
+        sx : 0,
+        sy : 0,
+        // frame size
+        sWidth : 242,
+        sHeight : 258
+    };
+
+    this.currentFrame;
+
     this.draw = function() {
         // draw
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, false);
-        c.moveTo(this.position.x, this.position.y);
-        c.lineTo(this.position.x, this.position.y-(this.radius*2));
-        c.lineWidth = 5;
-        c.strokeStyle = this.color;
-        c.stroke();
-        c.closePath();
+        c.drawImage(this.shipImage, this.position.x-30, this.position.y-30, 60, 60);
+        c.drawImage(this.flame.sprite,this.flame.sx,this.flame.sy,this.flame.sWidth,this.flame.sHeight,this.position.x-15,this.position.y+30,30,30);
     }
 
     this.update = function() {
-        // update        
+        // update
+        if (total_frames % 8 == 0) {
+            this.currentFrame = ++total_frames % 3;
+            this.flame.sx = this.currentFrame * this.flame.sWidth;
+        }
         this.draw();
     }
 
-    this.move = function() {
+    this.move = function(data,type) {
         if(!(this.position.x < 0 || this.position.x > width)) {
-            this.position.x = event.touches[0].clientX;
+            if(type) {
+                this.position.x = event.touches[0].clientX;
+                //this.flame.cx = this.position.x-15;
+            }
+            else {
+                this.position.x = event.clientX;
+            }
         }
     }
 }
@@ -49,43 +69,11 @@ function Ship(x,y,radius) {
 function Bullet(x) {
     this.position = {
         x : x,
-        y : height-100
+        y : height-130
     }
     this.radius = 3;
     this.dy = 5;
-    this.color = "#f00";
-    
-    this.draw = function() {
-        // draw
-        c.beginPath();
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, false);
-        c.strokeStyle = this.color;
-        c.stroke();
-        c.closePath();
-    }
-
-    this.update = function() {
-        // update
-        this.position.y -= this.dy;
-        this.draw()
-    }
-}
-
-
-/**
- * Bullet class
- *
- * @prop {object} position bullet's x and y position.
- * @prop {float | int} radius radius of enemy.
- */
-function Enemy(position,radius) {
-    this.position = {
-        x : position.x,
-        y : position.y
-    }
-    this.dy = (radius*1.3)/10;
-    this.color = "rgba(255,255,255,0.7)";
-    this.radius = radius;
+    this.color = "#ff0";
     
     this.draw = function() {
         // draw
@@ -98,15 +86,92 @@ function Enemy(position,radius) {
 
     this.update = function() {
         // update
+        this.position.y -= this.dy;
+        this.draw()
+    }
+}
+
+
+/**
+ * enemy class
+ *
+ * @prop {object} position enemy's x and y position.
+ * @prop {float | int} radius radius of enemy.
+ *
+ */
+function Enemy(position,radius) {
+    this.position = {
+        x : position.x,
+        y : position.y
+    }
+
+    this.radius = radius/2;
+    this.dy = 0.1*this.radius;
+
+    this.dim = {
+        width: this.radius*2,
+        height: this.radius*2
+    }
+
+    this.color = "#d5d5d5";
+
+    this.images = [document.getElementById('ast_1'),document.getElementById('ast_2'),document.getElementById('ast_3')];
+    this.asteroidImage = this.images[util.randomIntFromRange(0,2)];
+
+    this.draw = function() {
+        // draw
+
+        c.drawImage(this.asteroidImage, this.position.x-(this.radius), this.position.y-(this.radius), this.dim.width, this.dim.height);
+    }
+
+    this.update = function() {
+        // update
         this.position.y += this.dy;
         this.draw();
     }
 
     this.reset = function() {
-        this.position.x = util.randomIntFromRange(0+radius , width-radius);
+        this.position.x = util.randomIntFromRange(0+this.radius , width-this.radius);
         this.position.y = util.randomIntFromRange(-(height/2) ,0);
-        this.radius = util.randomIntFromRange(5,30);
-        this.dy = (this.radius*1.3)/10;
+        this.asteroidImage = this.images[util.randomIntFromRange(0,2)];
         this.update();
+    }
+}
+
+
+/**
+ * starField class
+ *
+ */
+
+function Star() {
+    this.radius = util.randomIntFromRange(1,2);
+    this.dr = 1;
+    this.dy = this.radius /5;
+    this.position = {
+        x : util.randomIntFromRange(0 , width),
+        y : util.randomIntFromRange(0 , height)
+    }
+
+    this.draw = function() {
+        // draw
+
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2, false);
+        c.fillStyle = "white";
+        c.fill();
+        c.closePath();
+    }
+    this.update = function() {
+        if(this.radius >= 2 || this.radius <= 1) {
+            this.dr = -(this.dr);
+        }
+        this.radius += (this.dr/50);
+        this.position.y += this.dy;
+        if(this.position.y > height) {
+            this.position.x = util.randomIntFromRange(0 , width);
+            this.position.y = -10;
+        }
+        this.draw();
     }
 }
